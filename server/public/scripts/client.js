46b330b6952e525ed7cd5e04636f8d3c11f37d33
elvishang <hangelvis@gmail.com>
2017-10-15 12:29:42 -0500

    completed weekend challenge 2

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,32 @@ console.log('js scourced')
 
 $(document).ready(onReady);
 
+var map = {
+    Add: '+',
+    Subtract: '-',
+    Multiplication: '*',
+    Division: '/'
+}
 
 function onReady() {
+    $('.calculate').on('click', calculateResult)
+}
+
+function calculateResult(event) {
+    event.preventDefault();
+    var data = {
+        x: $('.firstVal').val(),
+        y: $('.secondVal').val(),
+        type: $(this).attr('id')
+    };
     
+    $.ajax({
+        type: "POST",
+        url: "/calcuate",
+        data: data
+    }).done(function(response){
+        console.log(response);
+        $('.total').text(response);
+        $('tbody').append('<tr><td>' + data.x + ' ' + map[data.type] + ' ' + data.y + ' = ' + response + '</td></tr>');
+    })
 }
--- server/server.js
+++ server/server.js
@@ -6,3 +6,31 @@ var port = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.listen(port, function() {
+    console.log('listening to port: ', port)
+})
+
+app.post('/calcuate', function (req, res) {
+    var x = parseInt(req.body.x, 10),
+        y = parseInt(req.body.y, 10),
+        type = req.body.type, 
+        result;
+
+        console.log(req.body);
+    switch(type) {
+        case 'Add':
+        result = x + y;
+        break;
+        case 'Subtract':
+        result = x - y;
+        break;
+        case 'Multiplication':
+        result = x * y;
+        break;
+        case 'Division':
+        result = x / y;
+        break;
+    }
+
+    res.send(result + '');
+});
